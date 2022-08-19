047dad730e08626797df2e7748cbf0126c89548d
aneelbiswas <biswas.99@osu.edu>
2022-08-19 10:49:16 -0400

    Added taxonomy_scripts.py

---
+++ skyalign_scripts/docs/scripts/taxonomy_scripts.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python Python
+
+"""
+This script takes previously creates taxonomy files and represents them in trees
+
+@Author Aneel Biswas
+@Version 0.1
+@Date 2022Aug19
+"""
+
+from __future__ import annotations
+
+
+def main() -> None:
+    return None
+
+
+if __name__ == "__main__":
+    main()
