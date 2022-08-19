2a7bbbdc15754c95f6083890e1cec5b3fc5b1ac8
aneelbiswas <biswas.99@osu.edu>
2022-08-19 10:48:28 -0400

    Added taxonomy_scripts.py

--- skyalign_scripts/docs/scripts/multiple_align_to_skyalign.py
+++ skyalign_scripts/docs/scripts/multiple_align_to_skyalign.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python Python
 
 """
-This script takes fasta alignment files and creates hmm consensus logos using the sklign api
+This script takes fasta alignment files and creates hmm consensus logos using the skylign api
 
 @Author Aneel Biswas
-@Version 0.1
-@Date 2022Aug17
+@Version 1.0
+@Date 2022Aug18
 """
 
 from __future__ import annotations
@@ -22,19 +22,17 @@ INPUT_DIR = '/Users/aneelbiswas/PycharmProjects/skyalign_scripts/skyalign_script
 OUTPUT_DIR = '/Users/aneelbiswas/PycharmProjects/skyalign_scripts/skyalign_scripts/test_supplemental/test_outfiles'
 BATCH_SIZE = 50
 
-loop = asyncio.get_event_loop()
-
 
 async def skylign_post(skylign_url: str, header_dict: dict[str: str],
                        file_dict: dict[str: str], response_dict: dict[str: str]) -> None:
 
-    response_dict[file_dict['file']] = requests.post(skylign_url, headers=header_dict, files=file_dict)
+    response_dict[file_dict['file'][0]] = requests.post(skylign_url, headers=header_dict, files=file_dict)
 
     return None
 
 
 async def create_response_dict(batch_size, fasta_list) -> dict[str: requests.Response]:
-    skylign_url = 'http://sklign.org/'
+    skylign_url = 'http://skylign.org/'
     response_dict = {}
     batch_number = len(fasta_list) // BATCH_SIZE
 
@@ -42,23 +40,24 @@ async def create_response_dict(batch_size, fasta_list) -> dict[str: requests.Res
         tasks = []
         for i in range(batch_size):
             fasta = fasta_list[batch * batch_size + i]
-            tasks.append(loop.create_task(skylign_post(
+            tasks.append(asyncio.create_task(skylign_post(
                 skylign_url, {'Accept': 'application/json'},
-                {'file': f'{INPUT_DIR}/{fasta}', 'processing': 'hmm'}, response_dict)))
+                {'file': (f'{INPUT_DIR}/{fasta}', open(f'{INPUT_DIR}/{fasta}', 'rb'))}, response_dict)))
         await gather(*tasks, sleep(1))
     # If last batch is not batch size finishes remainder of list
     tasks = []
     for i in range(batch_size * batch_number, len(fasta_list)):
         fasta = fasta_list[i]
-        tasks.append(loop.create_task(skylign_post(
+        tasks.append(asyncio.create_task(skylign_post(
             skylign_url, {'Accept': 'application/json'},
-            {'file': f'{INPUT_DIR}/{fasta}', 'processing': 'hmm'}, response_dict)))
+            {'file': (f'{INPUT_DIR}/{fasta}', open(f'{INPUT_DIR}/{fasta}', 'rb'))}, response_dict)))
     await gather(*tasks, sleep(1))
 
     return response_dict
 
 
 async def skylign_get(get_url: str, fasta: str, json_responses_dict: dict) -> None:
+    get_url = get_url[:4] + get_url[5:]
     json_responses_dict[fasta] = requests.get(get_url, headers={'Accept': 'application/json'})
     return None
 
@@ -67,13 +66,15 @@ async def save_consensus_logos(response_dict: dict[str: requests.Response], outp
     json_responses_dict = {}
     tasks = []
     for fasta, response in response_dict.items():
+        print(response.json())
         tasks.append(
-            loop.create_task(
-                skylign_get(json.load(response.json())['url'], fasta, json_responses_dict)))
+            asyncio.create_task(
+                skylign_get(response.json()['url'], fasta, json_responses_dict)))
     await gather(*tasks)
-    for fasta, logo in json_responses_dict:
+    for fasta, logo in json_responses_dict.items():
+        print(fasta)
         with open(f'{output_dir}/{fasta.split(sep="/")[-1][:-6]}_logo.json', 'w', encoding='utf8') as json_file:
-            json_file.write(logo)
+            json.dump(logo.json(), json_file)
     return None
 
 
